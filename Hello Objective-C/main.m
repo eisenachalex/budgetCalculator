932e5256e57c0ea0de20094936a14a3bec986216
Alex Eisenach <hoopmiester@Alexs-MacBook-Pro-3.local>
2013-03-25 21:05:06 -0700

    Initial Commit

---
+++ Hello Objective-C/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Hello Objective-C
+//
+//  Created by Alex Eisenach on 3/25/13.
+//  Copyright (c) 2013 Alex Eisenach. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
